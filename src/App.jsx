f9c7ff39d32e38be62aeb2bb33150a772083842e
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-08-24 21:02:19 +0600

    action redux is running

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='container py-10 px-10 mx-0 min-w-full'>
+      <h2 className="bg-green-500 text-2xl font-bold">Love</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/redux/actionTypes.js
@@ -0,0 +1,10 @@
+// Left_Side - action >> Right_side - types 
+
+export const ADD_TODO = "ADD_TODO"
+export const TOGGLE_TODO = "TOGGLE_TODO"
+export const REMOVE_TODO = "REMOVE_TODO"
+export const MARK_COMPLETED = "MARK_COMPLETED"
+export const MARK_INCOMPLETE = "MARK_INCOMPLETE"
+export const FILTER_TODO = "FILTER_TODO"
+export const MARK_ALL_COMPLETED = "MARK_ALL_COMPLETED"
+export const UPDATE_TODO = "UPDATE_TODO"
